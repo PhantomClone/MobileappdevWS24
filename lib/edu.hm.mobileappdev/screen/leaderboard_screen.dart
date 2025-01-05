7df0a94accee5af58b037c35e41ab53f95a59fbd
Tobias Boeck <boeck.tobias@hm.edu>
2025-01-05 16:11:13 +0100

    leaderboard & reslut screen dummies

---
+++ lib/edu.hm.mobileappdev/screen/leaderboard_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:go_router/go_router.dart';
+
+class LeaderboardScreen extends StatefulWidget {
+  const LeaderboardScreen({super.key});
+
+  @override
+  _LeaderboardScreenState createState() => _LeaderboardScreenState();
+}
+
+class _LeaderboardScreenState extends State<LeaderboardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
---
+++ lib/edu.hm.mobileappdev/screen/result_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:go_router/go_router.dart';
+
+class ResultScreen extends StatefulWidget {
+  const ResultScreen({super.key});
+
+  @override
+  _ResultScreenState createState() => _ResultScreenState();
+}
+
+class _ResultScreenState extends State<ResultScreen> {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
