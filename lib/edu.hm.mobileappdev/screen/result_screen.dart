7917693763a7e5161523ac9c4db108862af80217
Tobias Boeck <boeck.tobias@hm.edu>
2025-01-06 15:22:34 +0100

    result screen missing scores

--- lib/edu.hm.mobileappdev/screen/result_screen.dart
+++ lib/edu.hm.mobileappdev/screen/result_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:mobileappdev/edu.hm.mobileappdev/model/kniffel_field.dart';
 import 'package:provider/provider.dart';
 import 'package:go_router/go_router.dart';
 
-//TODO nach Spielende mit Platzierung, New Game & To Leaderboard
+import '../state/play_state.dart';
 
 class ResultScreen extends StatefulWidget {
   const ResultScreen({super.key});
@@ -14,6 +15,65 @@ class ResultScreen extends StatefulWidget {
 class _ResultScreenState extends State<ResultScreen> {
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    final gameState = Provider.of<KniffelGameState>(context, listen: false);
+    final players = gameState.players;
+
+    final scores = players.map((player) {
+      //final totalScore = KniffelFieldExtension.calculateFinalScore(player.scoreCard);
+      final totalScore = 42;
+      return {
+        'player': player.name,
+        'score': totalScore
+      };
+    }).toList();
+
+    scores.sort((a, b) => (b['score']! as int) - (a['score'] as int));
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Results')),
+      body: Column(
+        children: [
+          Text(
+            'Game Over!',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: scores.length,
+              itemBuilder: (context, index) {
+                final result = scores[index];
+                return ListTile(
+                  title: Text(
+                    '${index + 1}. ${result['player']}',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  subtitle: Text('Score: ${result['score']} (Bonus: ${result['bonus']})'),
+                );
+              },
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () => context.go('/'),
+                child: Text('Home'),
+              ),
+              ElevatedButton(
+                onPressed: () => context.go('/score'),
+                child: Text('Go to Scoreboard'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  gameState.resetPlayersToStart(players);
+                  context.go('/play');
+                },
+                child: Text('Play Again'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
