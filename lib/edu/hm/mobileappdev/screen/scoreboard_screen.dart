e1f5efd0976221d919b2190a12599a702d7a7f0a
Tobias Boeck <boeck.tobias@hm.edu>
2025-01-14 11:58:31 +0100

    scoreboard design

--- lib/edu/hm/mobileappdev/screen/scoreboard_screen.dart
+++ lib/edu/hm/mobileappdev/screen/scoreboard_screen.dart
@@ -17,47 +17,136 @@ class _ScoreboardScreenState extends State<ScoreboardScreen> {
   @override
   void initState() {
     super.initState();
-    _playersFuture = Provider.of<PlayerRepository>(context, listen: false).getPlayersRanking(limit: 10);
+    _playersFuture = Provider.of<PlayerRepository>(context, listen: false)
+        .getPlayersRanking(limit: 10);
+  }
+
+  Color? _getRankColor(int index) {
+    if (index == 0) {
+      return Colors.amber;
+    } else if (index == 1) {
+      return Colors.grey;
+    } else if (index == 2) {
+      return Colors.deepOrangeAccent;
+    } else {
+      return Colors.grey[900];
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Scoreboard')),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () => context.go('/'),
-            child: const Text('Home'),
-          ),
-          Expanded(
-            child: FutureBuilder<List<PlayerDTO>>(
-              future: _playersFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return const Center(child: Text('No players available.'));
-                }
-
-                final players = snapshot.data!;
-                return ListView.builder(
-                  itemCount: players.length,
-                  itemBuilder: (context, index) {
-                    final player = players[index];
-                    return ListTile(
-                      leading: Text('#${index + 1}'),
-                      title: Text(player.name),
-                      trailing: Text('Score: ${player.score}'),
-                    );
-                  },
-                );
-              },
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background/dice_background.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.4),
+              BlendMode.darken,
             ),
           ),
-        ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 40),
+              Text(
+                'Hall of Fame',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amber[100],
+                  shadows: [
+                    Shadow(
+                      blurRadius: 10.0,
+                      color: Colors.black.withOpacity(0.5),
+                      offset: Offset(3.0, 3.0),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              Expanded(
+                child: FutureBuilder<List<PlayerDTO>>(
+                  future: _playersFuture,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Center(child: Text('Error: ${snapshot.error}'));
+                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                      return const Center(child: Text('No players available.'));
+                    }
+
+                    final players = snapshot.data!;
+                    return ListView.builder(
+                      itemCount: players.length,
+                      itemBuilder: (context, index) {
+                        final player = players[index];
+
+                        return Card(
+                          elevation: 10,
+                          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                          color: Colors.grey[800],
+                          child: ListTile(
+                            leading: CircleAvatar(
+                              backgroundColor: _getRankColor(index),
+                              child: Text(
+                                '${index + 1}',
+                                style: const TextStyle(color: Color(0xFFFFF8E1), fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            title: Text(
+                              player.name,
+                              style: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFFFFF8E1)
+                              ),
+                            ),
+                            subtitle: Text(
+                              'Score: ${player.score}',
+                              style: const TextStyle(
+                                fontSize: 18,
+                                color: Color(0xFFFFF8E1),
+                              ),
+                            ),
+                            trailing: index == 0
+                                ? Icon(
+                              Icons.emoji_events,
+                              color: Colors.amber,
+                              size: 30,
+                            )
+                                : null,
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+              ElevatedButton.icon(
+                onPressed: () => context.go('/'),
+                icon: Icon(Icons.home, size: 20),
+                label: const Text(
+                    'Home',
+                  style: const TextStyle(fontSize: 20),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.amber[100],
+                  foregroundColor: Colors.grey[900],
+                  padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
