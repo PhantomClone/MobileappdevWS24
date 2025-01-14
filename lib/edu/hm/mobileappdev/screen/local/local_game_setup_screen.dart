f397e4c716672a492e71ca764a0a8e830f7226e8
Tobias Boeck <boeck.tobias@hm.edu>
2025-01-14 17:16:26 +0100

    local game setup screen design

--- lib/edu/hm/mobileappdev/screen/local/local_game_setup_screen.dart
+++ lib/edu/hm/mobileappdev/screen/local/local_game_setup_screen.dart
@@ -16,17 +16,14 @@ class _LocalGameSetupScreenState extends State<LocalGameSetupScreen> {
   String? _errorMessage;
 
   void _addPlayerField() {
-    if (_playerControllers.any((controller) => controller.text.isEmpty)) {
-      setState(() {
-        _errorMessage = 'Bitte gib zuerst einen Namen ein.';
-      });
-      return;
-    }
-
-    if (_playerControllers.length < 4) {
+    if (_playerControllers.every((controller) => controller.text.isNotEmpty) && _playerControllers.length < 4) {
       setState(() {
         _playerControllers.add(TextEditingController());
         _errorMessage = null;
+      });
+    } else {
+      setState(() {
+        _errorMessage = 'Bitte gib zuerst alle anderen Namen ein.';
       });
     }
   }
@@ -43,14 +40,14 @@ class _LocalGameSetupScreenState extends State<LocalGameSetupScreen> {
         .map((controller) => Player(controller.text))
         .toList();
 
-    if (players.isNotEmpty) {
+    if (players.isEmpty) {
+      setState(() {
+        _errorMessage = '';
+      });
+    } else {
       final gameState = Provider.of<KniffelGameState>(context, listen: false);
       gameState.resetPlayers(players);
       context.go('/play');
-    } else {
-      setState(() {
-        _errorMessage = 'Zum Starten mindestens einen Spieler eingeben!';
-      });
     }
   }
 
@@ -65,88 +62,128 @@ class _LocalGameSetupScreenState extends State<LocalGameSetupScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Kniffel'),
-        backgroundColor: Colors.teal,
-        centerTitle: true,
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Card(
-              elevation: 4,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    ..._playerControllers.asMap().entries.map(
-                          (entry) {
-                        int index = entry.key;
-                        TextEditingController controller = entry.value;
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Expanded(
-                                child: TextField(
-                                  controller: controller,
-                                  decoration: InputDecoration(
-                                    labelText:
-                                    'Spieler #${index + 1}',
-                                    border: const OutlineInputBorder(),
-                                    filled: true,
-                                    fillColor: Colors.white,
-                                  ),
-                                ),
-                              ),
-                              if (_playerControllers.length > 1)
-                                IconButton(
-                                  icon: const Icon(
-                                    Icons.close,
-                                    color: Colors.red,
-                                  ),
-                                  onPressed: () => _removePlayerField(index),
-                                ),
-                            ],
-                          ),
-                        );
-                      },
-                    ),
-                    if (_errorMessage != null)
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: Text(
-                          _errorMessage!,
-                          style: const TextStyle(color: Colors.red),
-                        ),
-                      ),
-                    if (_playerControllers.length < 4)
-                      _buildButton(
-                          label: "Spieler hinzufügen",
-                          color: Colors.teal,
-                          onPressed: _addPlayerField),
-                    const SizedBox(height: 100),
-                    _buildButton(
-                      label: 'Spiel starten',
-                      color: Colors.blue,
-                      onPressed: () => _startLocalGame(context),
-                    ),
-                    const SizedBox(height: 16),
-                    _buildButton(
-                      label: 'Zurück',
-                      color: Colors.orange,
-                      onPressed: () => context.go('/'),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background/dice_background.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.3), BlendMode.darken),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 40),
+              Text(
+                'Setup Lokales Spiel',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amber[100],
+                  shadows: [
+                    Shadow(
+                      blurRadius: 10.0,
+                      color: Colors.black.withOpacity(0.5),
+                      offset: Offset(3.0, 3.0),
                     ),
                   ],
                 ),
+                textAlign: TextAlign.center,
               ),
-            ),
+              const SizedBox(height: 156),
+
+              _buildPlayerSelection(),
+
+              Expanded(child: Container()),
+              _buildButton(
+                label: 'Spiel starten',
+                color: Color(0xFFFFECB3),
+                textColor: Color(0xFF212121),
+                onPressed: () => _startLocalGame(context),
+              ),
+              const SizedBox(height: 16),
+              _buildButton(
+                icon: Icons.arrow_back,
+                label: 'Zurück',
+                color: Color(0xFFFFECB3),
+                onPressed: () => context.go('/'),
+                textColor: Colors.grey[900],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPlayerSelection() {
+    return Center(
+      child: Card(
+        elevation: 10,
+        color: Color.fromRGBO(70, 70, 70, 0.7), // Graue Card mit 70% Transparenz
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Gib die Namen ein:",
+                style: TextStyle(fontSize: 30, color: Color(0xFFFFECB3), fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              ..._playerControllers.asMap().entries.map((entry) {
+                int index = entry.key;
+                TextEditingController controller = entry.value;
+
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: controller,
+                          style: const TextStyle(color: Color(0xFFFFECB3), fontSize: 20), // Textfarbe auf weiß ändern
+                          decoration: InputDecoration(
+                            labelText: 'Spieler #${index + 1}',
+                            labelStyle: const TextStyle(color: Color(0xFFFFECB3), fontSize: 18),
+                            border: const OutlineInputBorder(),
+                            filled: true,
+                            fillColor: Colors.grey[850],
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xFFFFECB3)),
+                            ),
+                          ),
+                        ),
+                      ),
+                      if (_playerControllers.length > 1)
+                        IconButton(
+                          icon: const Icon(Icons.close, color: Colors.red, size: 30),
+                          onPressed: () => _removePlayerField(index),
+                        ),
+                    ],
+                  ),
+                );
+              }),
+              if (_errorMessage != null)
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text(
+                    _errorMessage!,
+                    style: const TextStyle(color: Colors.red, fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              if (_playerControllers.length < 4)
+                _buildButton(
+                  label: "Zusätzlichen Spieler hinzufügen",
+                  color: Color(0xFF212121),
+                  onPressed: _addPlayerField,
+                ),
+            ],
           ),
         ),
       ),
@@ -157,20 +194,23 @@ class _LocalGameSetupScreenState extends State<LocalGameSetupScreen> {
     required String label,
     required Color color,
     required VoidCallback onPressed,
+    Color? textColor = Colors.white,
+    IconData? icon,
   }) {
-    return ElevatedButton(
+    return ElevatedButton.icon(
       style: ElevatedButton.styleFrom(
         backgroundColor: color,
-        foregroundColor: Colors.white,
+        foregroundColor: textColor,
         padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(8),
         ),
       ),
       onPressed: onPressed,
-      child: Text(
+      icon: icon != null ? Icon(icon, size: 20) : null,
+      label: Text(
         label,
-        style: const TextStyle(fontSize: 16),
+        style: const TextStyle(fontSize: 20),
       ),
     );
   }
